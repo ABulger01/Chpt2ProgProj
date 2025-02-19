17522a5c0de4dd3e5084961432fd09c303246203
ABulger01 <Nyesha2001@gmail.com>
2025-02-19 23:46:48 +0000

    added program and made corrections

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -2,20 +2,16 @@
 using namespace std;
 
 int main() { 
-// Declare the gallons per tank and the miles per gallon
-  const double  gallonsPerTank= 20.0;
+// Declare the amount of customers
+  int amountOfCustomersSurveyed = 16500;
+  double percentOfEnergyDrinks = .15;
+  double percentOfCitursDrinks= .58;
 
-  // Declare the miles per tank
- const double miles1 = 23.5; 
-  const double miles2 = 28.9;
-
-  // Calculate distances for each fuel efficiency
-  double distanceOfTravel1 = gallonsPerTank * miles1;
-  double distanceOfTravel2 = gallonsPerTank * miles2;
-
-  //Display the distances
-  cout << "The distance of travel for 1 gas tank in town is " << distanceOfTravel1 << " miles." << endl;
-  cout << "The distance of travel for 1 gas tank on the highway is " << distanceOfTravel2 << " miles." << endl;
+  int customersAmount = amountOfCustomersSurveyed * percentOfEnergyDrinks;
+  int customersAmount2 = customersAmount * percentOfCitursDrinks;
+  cout << "The approximate number of customers in the survery who purchased one of more energy drinks per week is " << customersAmount << "." << endl;
+  
+  cout << "The approxiate number of customers in the survey who perfer citurs flavored drinks is " << customersAmount2 << ". "  << endl;
 
   return 0;
    
