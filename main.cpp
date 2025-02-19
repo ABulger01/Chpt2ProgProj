309283def73b5d2bfe5a2e3cfa192937253eaebe
ABulger01 <Nyesha2001@gmail.com>
2025-02-19 23:11:41 +0000

    added program and fixed any mistakes

--- main.cpp
+++ main.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 
 int main() { 
-// Declare oceans level rise per year in mm
-  const double OceanRisePerYear = 1.5; 
- int  years1 = 5;
-  int years2 =7;
-  int years3 = 10;
-  double oceanRise5 = OceanRisePerYear * years1;
- double oceanRise7 = OceanRisePerYear * years2;
-  double oceanRise10 = OceanRisePerYear * years3;
-  cout << "In 5 years the ocean will rise " << oceanRise5 <<  "mm" << endl;
-  cout << "In 7 years the ocean will rise " << oceanRise7 <<  "mm" << endl;
-  cout << "In 10 years the ocean will rise " << oceanRise10 <<  "mm" << endl;
+// Declare the gallons per tank and the miles per gallon
+  const double  gallonsPerTank= 20.0;
+
+  // Declare the miles per tank
+ const double miles1 = 23.5; 
+  const double miles2 = 28.9;
+
+  // Calculate distances for each fuel efficiency
+  double distanceOfTravel1 = gallonsPerTank * miles1;
+  double distanceOfTravel2 = gallonsPerTank * miles2;
+
+  //Display the distances
+  cout << "The distance of travel for 1 gas tank in town is " << distanceOfTravel1 << " miles." << endl;
+  cout << "The distance of travel for 1 gas tank on the highway is " << distanceOfTravel2 << " miles." << endl;
+
   return 0;
-  
+   
 }
